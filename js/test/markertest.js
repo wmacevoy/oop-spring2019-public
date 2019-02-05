937b74fcf00311af8850ea87fffed9cc8c7323d4
Warren MacEvoy <wmacevoy@gmail.com>
2019-02-05 13:26:35 -0700

    test marker

--- js/test/markertest.js
+++ js/test/markertest.js
@@ -1,18 +1,23 @@
+"use strict";
+
 var assert = require('assert');
-var marker = require('marker');
+var chai = require('chai');
+var marker = require('../marker');
+
+var expect = chai.expect;
 
 describe('Marker', function() {
   describe('#constructor()', function() {
     it('should be black when started black', function(){
-        let blackMarker = marker.Marker("black");
+        let blackMarker = new marker.Marker("black");
         assert.equal("black",blackMarker.color);
     });
     it('should be initially capped', function() {
-        let blackMarker = marker.Marker("black");
+        let blackMarker = new marker.Marker("black");
         assert.equal(true,blackMarker.capped);        
     });
     it('should fail to draw when capped', function() {
-        let blackMarker = marker.Marker("black");
+        let blackMarker = new marker.Marker("black");
         expect(blackMarker.draw().to.throw(Marker.BAD_MARKER_CAPPED_DRAW));
     });
   });
